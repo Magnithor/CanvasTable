b076bf52c0a1f0af40167c6376aec29a0ead8713
Magni Birgisson <magni@samsyn.is>
2020-04-08 23:31:45 +0000

    testing

---
+++ Testing/test/test.spec.ts
@@ -0,0 +1,8 @@
+describe("A suite", () => {
+  it("contains spec with an expectation", () => {
+    expect(true).toBe(true);
+  });
+  it("contains spec with an expectation 2", () => {
+    expect(true).toBe(!false);
+  });
+});
